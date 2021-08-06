4fb5219efe5c5af49e85ddd01e268c31b71bb6f6
Leia Scofield <leia@leiascofield.com>
2021-08-06 16:39:01 -0500

    Remove requirement from Display name in Account Details

---
+++ remove-display-name-requirement.php
@@ -0,0 +1,6 @@
+// Remove requirement from Display name in Account Details
+add_filter('woocommerce_save_account_details_required_fields', 'wc_save_account_details_required_fields' );
+function wc_save_account_details_required_fields( $required_fields ){
+    unset( $required_fields['account_display_name'] );
+    return $required_fields;
+}
